52eae0799c2a9cf77bad98753f390a9310835ae2
joonkyu <csus92@gmail.com>
2018-12-02 18:47:49 +0900

    최종테스트 커밋

--- LSTM/Classifier2.py
+++ LSTM/Classifier2.py
@@ -4,16 +4,17 @@ import numpy as np
 import LSTM.Bi_LSTM as Bi_LSTM
 import LSTM.Word2Vec as Word2Vec
 from konlpy.tag import Okt
+from eunjeon import Mecab
 
 W2V = Word2Vec.Word2Vec()
-
+mecab = Mecab()
 okt = Okt()
 Batch_size = 1
 Vector_size = 300
-Maxseq_length = 922  ## Max length of training data
+Maxseq_length = 2000  ## Max length of training data
 learning_rate = 0.001
 lstm_units = 128
-num_class = 8
+num_class = 4
 keep_prob = 1.0
 tf.reset_default_graph()
 X = tf.placeholder(tf.float32, shape=[None, Maxseq_length, Vector_size], name='X')
@@ -30,7 +31,7 @@ prediction = tf.nn.softmax(logits)
 
 saver = tf.train.Saver()
 init = tf.global_variables_initializer()
-modelName = "models/Bidirectional_LSTM/BiLSTM_model.ckpt"
+modelName = "Jupyter/BiLSTM_mecab_morphs_eqaul_4/BiLSTM_model.ckpt"
 
 sess = tf.Session()
 sess.run(init)
@@ -38,20 +39,21 @@ saver.restore(sess, modelName)
 
 def classify(jss):
     okt = Okt()
-    tokens = okt.nouns(jss)
-    embedding = W2V.Convert2Vec('models/word2vec_okt_nouns2.model', tokens)
+    #tokens = okt.nouns(jss)
+    tokens = mecab.nouns(jss)
+    embedding = W2V.Convert2Vec('data/word2vec_mecab_morphs2.model', tokens)
     zero_pad = W2V.Zero_padding(embedding, Batch_size, Maxseq_length, Vector_size)
     global sess
     result = sess.run(tf.argmax(prediction,1), feed_dict = {X: zero_pad , seq_len: [len(tokens)] } )
     # print(sess.run(prediction, feed_dict = {X: zero_pad , seq_len: [len(tokens)] } ) )
     if result == 0:
-        return '경영지원'
+        return '기타'
     elif result == 1:
-        return '재무·금융'
+        return '재무'
     elif result == 2:
-        return '생산·물류'
+        return '응용프로그래밍'
     elif result == 3:
-        return '영업·마케팅'
+        return '응용프로그래밍'
     elif result == 4:
         return '인사'
     elif result == 5:
